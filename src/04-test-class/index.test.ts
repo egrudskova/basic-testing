8464faab0e4e23912277a1318cb09008934daa90
Elina <elina50505051@yandex.ru>
2024-10-27 01:34:44 +0300

    feat(04-test-class): add tests

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,55 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { getBankAccount, InsufficientFundsError, TransferFailedError } from '.';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(getBankAccount(10).getBalance()).toEqual(10);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    expect(() => getBankAccount(10).withdraw(11)).toThrowError(
+      InsufficientFundsError,
+    );
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    expect(() =>
+      getBankAccount(10).transfer(11, getBankAccount(10)),
+    ).toThrowError(InsufficientFundsError);
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    const bankAcc = getBankAccount(10);
+    expect(() => bankAcc.transfer(1, bankAcc)).toThrowError(
+      TransferFailedError,
+    );
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    expect(getBankAccount(10).deposit(10).getBalance()).toBe(20);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    expect(getBankAccount(10).withdraw(5).getBalance()).toBe(5);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const backAcc1 = getBankAccount(10);
+    const backAcc2 = getBankAccount(0);
+    backAcc1.transfer(5, backAcc2);
+    expect(backAcc1.getBalance()).toBe(5);
+    expect(backAcc2.getBalance()).toBe(5);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    //TODO
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    //TODO
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    //TODO
   });
 });
